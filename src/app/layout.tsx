2937338434d6fb6d532a25b54f7bd29fc64d8fd8
Alexandros Lekkas <alexandros.lekkas@outlook.com>
2025-03-24 20:18:28 -0500

    chore: First commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,44 @@
+import type { Metadata } from "next";
+import { Figtree } from "next/font/google";
+
+import { ThemeProvider } from "@/lib/providers/theme-provider";
+import { AuthProvider } from "@/lib/providers/auth-provider";
+
+import { Toaster } from "@/components/ui/sonner";
+
+import "./globals.css";
+
+const figtree = Figtree({
+  subsets: ["latin"],
+  variable: "--font-figtree",
+});
+
+export const metadata: Metadata = {
+  title: "Next Supa Shad Boilerplate",
+  description: "A boilerplate for Next.js with Supabase and Shadcn/UI",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <AuthProvider>
+        <body className={`${figtree.className} antialiased`}>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+            storageKey="theme-preference"
+          >
+            {children}
+          </ThemeProvider>
+          <Toaster />
+        </body>
+      </AuthProvider>
+    </html>
+  );
+}
